3a2c2f956bcb9a70f38b8a9794003335d1a3fa95
Maryana Dey <Dey.maryana1989@gmail.com>
2023-02-26 22:02:19 +0100

    add axios router-dom

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+import { Home } from 'pages/Home';
+
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template1
+//     </div>
+//   );
+// };
---
+++ src/pages/Other.js
@@ -0,0 +1,8 @@
+// import { useEffect } from 'react';
+// import axios from 'axios';
+
+export const Other = () => {
+    return (
+        <p>Other</p>
+    );
+};
